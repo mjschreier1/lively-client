186849b5146992c47eca67607d192c1d32707c3d
Michael Schreier <mjschreier1@gmail.com>
2018-05-11 12:28:53 -0600

    POST request to /events works, though with a parse error

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -5,9 +5,9 @@ import { AuthenticationService } from '../services/authentication.service';
 import { User } from '../interfaces/user';
 
 @Component({
-  selector: 'app-calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  selector: "app-calendar",
+  templateUrl: "./calendar.component.html",
+  styleUrls: ["./calendar.component.css"]
 })
 export class CalendarComponent implements OnInit {
   user: User;
@@ -18,15 +18,19 @@ export class CalendarComponent implements OnInit {
   startYear: number = this.moment.getFullYear();
   startMonth: any = this.moment.getMonth() + 1;
   startDate: any = this.moment.getDate();
-  startHour: number = this.moment.getHours();
+  startHour: number = this.moment.getHours() > 12
+    ? this.moment.getHours() - 12
+    : this.moment.getHours();
   startMinute: any = this.moment.getMinutes();
   endYear: number = this.momentPlusTwoHours.getFullYear();
   endMonth: any = this.momentPlusTwoHours.getMonth() + 1;
   endDate: any = this.momentPlusTwoHours.getDate();
-  endHour: number = this.momentPlusTwoHours.getHours();
+  endHour: number = this.momentPlusTwoHours.getHours()
+    ? this.momentPlusTwoHours.getHours() - 12
+    : this.momentPlusTwoHours.getHours();
   endMinute: any = this.momentPlusTwoHours.getMinutes();
-  startPm: boolean = this.startHour > 11;
-  endPm: boolean = this.endHour > 11;
+  startPm: boolean = this.moment.getHours() > 11;
+  endPm: boolean = this.momentPlusTwoHours.getHours() > 11;
 
   monthValidator: Object = {};
   monthOptions: Array<String> = [];
@@ -37,44 +41,64 @@ export class CalendarComponent implements OnInit {
   hourOptions: Array<number> = [];
   minuteOptions: Array<string> = [];
 
+  name: string;
+  location: string;
+  description: string;
+
   constructor(
     private _httpService: HttpService,
     private _authentication: AuthenticationService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this._authentication.user.subscribe(user => {
       this.user = user;
-      if(user.admin) {
-        while(this.startMinute % 5 !== 0) {
-          if(this.startMinute > 55) {
+      if (user.admin) {
+        while (this.startMinute % 5 !== 0) {
+          if (this.startMinute > 55) {
             this.startMinute = 55;
             this.endMinute = 55;
           } else {
-            this.startMinute < 9 ? this.startMinute = `0${this.startMinute.valueOf() + 1}` : this.startMinute++;
+            this.startMinute < 9
+              ? (this.startMinute = `0${this.startMinute.valueOf() + 1}`)
+              : this.startMinute++;
             this.endMinute = this.startMinute;
           }
         }
         this.setMonthValidator();
         this.setDateValidator(this.startMonth, this.startYear, true);
         this.setDateValidator(this.endMonth, this.endYear, false);
-        if(this.startMonth < 10) { this.startMonth = `0${this.startMonth}` }
-        if(this.startDate < 10) { this.startDate = `0${this.startDate}` }
-        if(this.endMonth < 10) { this.endMonth = `0${this.endMonth}` }
-        if(this.endDate < 10) { this.endDate = `0${this.endDate}` }
-        for(let i = 1; i < 13; i++) { this.hourOptions.push(i) }
-        for(let i = 0; i < 60; i+= 5) { i < 10 ? this.minuteOptions.push(`0${i}`) : this.minuteOptions.push(`${i}`) }
+        if (this.startMonth < 10) {
+          this.startMonth = `0${this.startMonth}`;
+        }
+        if (this.startDate < 10) {
+          this.startDate = `0${this.startDate}`;
+        }
+        if (this.endMonth < 10) {
+          this.endMonth = `0${this.endMonth}`;
+        }
+        if (this.endDate < 10) {
+          this.endDate = `0${this.endDate}`;
+        }
+        for (let i = 1; i < 13; i++) {
+          this.hourOptions.push(i);
+        }
+        for (let i = 0; i < 60; i += 5) {
+          i < 10
+            ? this.minuteOptions.push(`0${i}`)
+            : this.minuteOptions.push(`${i}`);
+        }
       }
-    })
+    });
 
     this._httpService.getEvents().subscribe(events => {
       events = events.map(event => {
         event.start = new Date(event.start);
         event.finish = new Date(event.finish);
         return event;
-      })
+      });
       this.events = events;
-    })
+    });
   }
 
   setMonthValidator() {
@@ -87,23 +111,41 @@ export class CalendarComponent implements OnInit {
 
   setDateValidator(month, year, start) {
     let daysInMonth = 31;
-    if(month === 4 || month === 6 || month === 9 || month === 11) {
+    if (month === 4 || month === 6 || month === 9 || month === 11) {
       daysInMonth = 30;
-    } else if(month === 2) {
+    } else if (month === 2) {
       daysInMonth = 28;
-      if(year % 4 === 0) { daysInMonth++ }
+      if (year % 4 === 0) {
+        daysInMonth++;
+      }
     }
-    for(let i = 1; i < daysInMonth + 1; i++) {
+    for (let i = 1; i < daysInMonth + 1; i++) {
       let key = i < 10 ? `0${i}` : `${i}`;
-      if(start) {
+      if (start) {
         this.startDateValidator[key] = i;
         this.startDateOptions.push(key);
-      }
-      else {
+      } else {
         this.endDateValidator[key] = i;
         this.endDateOptions.push(key);
       }
     }
   }
 
+  addEvent() {
+    this._httpService.addEvent({
+      name: this.name,
+      location: this.location,
+      description: this.description,
+      startYear: this.startYear,
+      startMonth: this.startMonth,
+      startDate: this.startDate,
+      startHour: this.startPm && this.startHour !== 12 ? this.startHour + 11 : this.startHour - 1,
+      startMinute: this.startMinute,
+      finishYear: this.endYear,
+      finishMonth: this.endMonth,
+      finishDate: this.endDate,
+      finishHour: this.endPm && this.endHour !== 12 ? this.endHour + 11 : this.endHour - 1,
+      finishMinute: this.endMinute
+    }).subscribe(res => { console.log(res) })
+  }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Event } from '../interfaces/event';
 import { User } from '../interfaces/user';
 
@@ -15,6 +15,23 @@ export class HttpService {
 
   getEvents(): Observable<Array<Event>> {
     return this._http.get<Array<Event>>(this.eventsUrl)
+  }
+
+  addEvent(data): Observable<string> {
+    let params = new HttpParams()
+    Object.keys(data).forEach(key => {
+      if(key !== "name" && key !== "location" && key !== "description") {
+        params = params.append(key.toString(), data[key].toString())
+      }
+    })
+    return this._http.post<string>(this.eventsUrl,
+      {
+        name: data.name,
+        location: data.location,
+        description: data.description || ""
+      },
+      { params }
+    )
   }
 
   authenticateUser(last, pin): Observable<User> {
